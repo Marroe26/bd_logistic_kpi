e3d3174f052d31b89f4cc09d81bf1515ebd2acbf
Marroe26 <mtomapasca@gmail.com>
2025-08-19 23:51:57 -0500

    create tipo movimiento/unidad medida

--- ddl/create_database.sql
+++ ddl/create_database.sql
@@ -1,2 +1,17 @@
+-- Creación base de datos
+CREATE DATABASE bd_logistic_kpi;
+-- DROP DATABASE bd_logistic_kpi;
 
-DROP DATABASE bd_logistic_kpi;
+USE bd_logistic_kpi;
+
+-- Creación de tablas
+	-- Tipos de indicador
+CREATE TABLE tipo_movimiento (
+id INT IDENTITY (1,1) PRIMARY KEY,
+nombre VARCHAR (20) UNIQUE NOT NULL,
+descripcion VARCHAR (MAX) NULL
+);
+
+
+-- Para verificar los detalles de las tablas
+EXEC sp_help tipo_movimiento
